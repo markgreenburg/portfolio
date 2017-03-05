f195eeb91949a299ddc4c7b32470b13203b0516a
markgreenburg <marek.greenburg@gmail.com>
2017-03-05 00:42:11 -0600

    Adding portfolio starter

---
+++ __init__.py
@@ -0,0 +1,19 @@
+"""
+Main Flask routes and app file for portfolio
+"""
+import sys
+from flask import Flask, render_template, url_for
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+APP = Flask(__name__)
+
+@APP.route("/")
+def home():
+    """
+    Homepage route, displays homepage
+    """
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    APP.run()
