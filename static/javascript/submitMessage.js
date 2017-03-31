aa2d5fd7478b386a2921ec7cca37d65e692bf385
markgreenburg <marek.greenburg@gmail.com>
2017-03-31 13:58:21 -0500

    UI tweaks for pending messages

--- static/javascript/submitMessage.js
+++ static/javascript/submitMessage.js
@@ -1,7 +1,10 @@
 'use strict';
 window.onload = () => {
-    $("#contact-form").on("submit", (event) => {
+    const contactForm = $("#contact-form")
+    contactForm.on("submit", (event) => {
         event.preventDefault();
+        const button = $("button#submit-btn");
+        button.html("(This might take a sec) <i class='fa fa-spinner fa-spin'></i>");
         const formData = {
             "name": $("input#name").val(),
             "email": $("input#email").val(),
@@ -12,8 +15,28 @@ window.onload = () => {
             url: "/submit",
             data: formData,
             encode: true,
-            success: (response) => { console.log(response) },
-            error: (err) => { console.log(err) }
+            success: (response) => {
+                // 1. Clear the form
+                contactForm.trigger("reset");
+                // 2. Show a confirmation
+                button.html("Sent! <i class='fa fa-check-circle'></i>");
+                // 3. Hide the form again(?)
+            },
+            error: (err) => {
+                // 1. Show an error message
+                // 2. Log the error
+            }
         });
     });
+};
+
+/* Toggles visibility of the contact form */
+const toggleForm = () => {
+    const contactForm = document.getElementById("contact-form-div");
+    const { display } = contactForm.style;
+    if (display === 'none' || !display) {
+        contactForm.style.display = 'inline-block';
+    } else {
+        contactForm.style.display = 'none';
+    }
 };
